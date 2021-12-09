0fe98ece25c25cca06e39ef7593514cebcd87763
Muthumani117 <95862617+Muthumani117@users.noreply.github.com>
2021-12-09 23:05:31 +0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,20 @@
+import fitz   # PyMUPdf library
+import xlsxwriter
+
+# pdf_filename = "/Users/mani/Desktop/keppel-corporation-limited-annual-report-2018.pdf"
+pdf_filename = input("Enter the pdf file path:")
+excel_sheet_name = input("Enter excel file name to export the text [Example test1.xlsx]:")
+
+workbook = xlsxwriter.Workbook(excel_sheet_name)        #create file
+
+doc = fitz.open(pdf_filename)
+for page in doc:
+        blocks = page.get_text("blocks")
+        blocks.sort(key = lambda block: block[0])  # sort vertically ascending
+        row_iter = 0
+        col = 0
+        worksheet = workbook.add_worksheet()
+        for b in blocks:
+                worksheet.write(row_iter, col, b[4].replace('\n', ''))
+                row_iter+=1
+workbook.close()
