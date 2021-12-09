29dc2df93b01ba6d501836203233de5ee5c254fb
Muthumani117 <95862617+Muthumani117@users.noreply.github.com>
2021-12-09 23:06:50 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import fitz   # PyMUPdf library
 import xlsxwriter
 
-# pdf_filename = "/Users/mani/Desktop/keppel-corporation-limited-annual-report-2018.pdf"
+# pdf_filename = "http://www.kepcorp.com/annualreport2018/pdf/keppel-corporation-limited-annual-report-2018.pdf"
 pdf_filename = input("Enter the pdf file path:")
 excel_sheet_name = input("Enter excel file name to export the text [Example test1.xlsx]:")
 
@@ -10,7 +10,7 @@ workbook = xlsxwriter.Workbook(excel_sheet_name)        #create file
 doc = fitz.open(pdf_filename)
 for page in doc:
         blocks = page.get_text("blocks")
-        blocks.sort(key = lambda block: block[0])  # sort vertically ascending
+        blocks.sort(key = lambda block: block[0])  # sort vertically ascending for natural reading order
         row_iter = 0
         col = 0
         worksheet = workbook.add_worksheet()
